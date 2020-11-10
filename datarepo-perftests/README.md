1e35593b35181d88622ac0a603fcf23e46d868db
Ivan <ichang@broadinstitute.org>
2020-11-09 22:12:30 -0500

    Using Terra Test Runner library to write a simple TDR client test.

---
+++ datarepo-perftests/README.md
@@ -0,0 +1,6 @@
+# datarepo-perftests
+Standalone sub-project of the jade-data-repo repository for running perf tests with the Terra Test Runner library.
+
+If this project does not show up in the IntelliJ Gradle Tool Window.
+Use the + button and select the build.gradle to link this project.
+Right click the java and resources directories and marked them as Sources and Resources root, respectively.
---
+++ datarepo-perftests/src/main/java/scripts/testscripts/ServiceStatus.java
@@ -0,0 +1,26 @@
+package scripts.testscripts;
+
+import bio.terra.datarepo.api.UnauthenticatedApi;
+import bio.terra.datarepo.client.ApiClient;
+import bio.terra.testrunner.runner.TestScript;
+import bio.terra.testrunner.runner.config.TestUserSpecification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scripts.utils.DataRepoUtils;
+
+public class ServiceStatus extends TestScript {
+  private static final Logger logger = LoggerFactory.getLogger(ServiceStatus.class);
+
+  /** Public constructor so that this class can be instantiated via reflection. */
+  public ServiceStatus() {
+    super();
+  }
+
+  public void userJourney(TestUserSpecification testUser) throws Exception {
+    ApiClient apiClient = DataRepoUtils.getClientForTestUser(testUser, server);
+    UnauthenticatedApi unauthenticatedApi = new UnauthenticatedApi(apiClient);
+
+    int httpStatus = unauthenticatedApi.serviceStatusWithHttpInfo().getStatusCode();
+    logger.debug("Service status: {}", httpStatus);
+  }
+}
